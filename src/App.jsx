5d38c373ada661a644f810a66e46c50f9f820563
umutefecell <umutcelik0662@gmail.com>
2025-09-12 10:28:36 +0300

    First version of sport sellect

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useState} from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [sport,setSport] = useState("");
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Sport Stats App</h1>
+      <select value={sport} onChange={(e) => setSport(e.target.value)}>
+        <option value="">---Select Sport for Stats---</option>
+        <option value="basketball">Basketball</option>
+        <option value="football">Football</option>
+      </select>
+      <p>Selected Sport: {sport || "None"}</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
