b9f2fafe9a33e03d8e3baca2f3ad176e042dd575
umutefecell <umutcelik0662@gmail.com>
2025-09-15 09:36:32 +0300

    Commit

--- src/components/TeamSelect.jsx
+++ src/components/TeamSelect.jsx
@@ -5,7 +5,11 @@ function TeamSelect({ value, onChange, options }) {
     return (
         <>
             <p></p>
+<<<<<<< Updated upstream
             <label>Team: </label>
+=======
+            <label> Team: </label>
+>>>>>>> Stashed changes
             <select value={value} onChange={ (e) => onChange(e.target.value)}>
                 <option value="">-- Select a Team --</option>
                 {safe.map( (t) => (<option key={t.id} value={t.id}>{t.name}</option>))}
